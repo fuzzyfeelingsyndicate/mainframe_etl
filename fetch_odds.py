2ffe22812121a64ae4de9737d13e059a6139d982
fuzzyfeelingsyndicate <fuzzyfeelingcompany@gmail.com>
2025-09-17 16:33:11 +0200

    Update fetch_odds.py

--- fetch_odds.py
+++ fetch_odds.py
@@ -1,2 +1,21 @@
+import os
+import psycopg2
 
-print('workflow ran successfully')
+def main():
+    conn = psycopg2.connect(
+        host=os.getenv("SUPABASE_HOST"),
+        dbname=os.getenv("SUPABASE_DB"),
+        user=os.getenv("SUPABASE_USER"),
+        password=os.getenv("SUPABASE_PASS"),
+        port=5432
+    )
+    cur = conn.cursor()
+
+    cur.execute('select * from "testTable"')
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
