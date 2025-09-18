30bae32ec23c984117b1b1fda2be063e8a72248e
fuzzyfeelingsyndicate <fuzzyfeelingcompany@gmail.com>
2025-09-18 15:43:57 +0200

    Update fetch_odds.py

--- fetch_odds.py
+++ fetch_odds.py
@@ -1,12 +1,18 @@
 import os
-#import psycopg2
+import psycopg2
 
-print(os.getenv("SUPABASE_HOST"))
-print(os.getenv("SUPABASE_DB"))
-print(os.getenv("SUPABASE_USER"))
-print(os.getenv("SUPABASE_PASS"))
-
-
+try:
+    conn = psycopg2.connect(
+        host=os.getenv("SUPABASE_HOST"),
+        dbname=os.getenv("SUPABASE_DB"),
+        user=os.getenv("SUPABASE_USER"),
+        password=os.getenv("SUPABASE_PASS"),
+        port=5432
+    )
+    print("✅ Connection successful")
+    conn.close()
+except Exception as e:
+    print("❌ Connection failed:", e)
 
 '''def main():
     conn = psycopg2.connect(
