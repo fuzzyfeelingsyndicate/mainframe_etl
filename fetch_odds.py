d6ddb4883afd643a0d12f64f73a2479a4349e7d5
Modith Hadya <40522163+PHNX-MOD@users.noreply.github.com>
2025-09-18 10:01:05 +0200

    Update fetch_odds.py

--- fetch_odds.py
+++ fetch_odds.py
@@ -1,7 +1,14 @@
 import os
-import psycopg2
+#import psycopg2
 
-def main():
+print(os.getenv("SUPABASE_HOST"))
+print(os.getenv("SUPABASE_DB"))
+print(os.getenv("SUPABASE_USER"))
+print(os.getenv("SUPABASE_PASS"))
+
+
+
+'''def main():
     conn = psycopg2.connect(
         host=os.getenv("SUPABASE_HOST"),
         dbname=os.getenv("SUPABASE_DB"),
@@ -18,4 +25,4 @@ def main():
     conn.close()
 
 if __name__ == "__main__":
-    main()
+    main() '''
