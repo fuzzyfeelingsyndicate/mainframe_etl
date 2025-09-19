caa9f3d4cf347a5c72719cfa5244af5dda1b7976
modithhadya <modith.hadya@gmail.com>
2025-09-19 17:10:43 +0200

    fxiign fetch_odds api

--- fetch_odds.py
+++ fetch_odds.py
@@ -4,16 +4,20 @@ import random
 
 id_random = random.randint(1,10000)
 
+DBNAME = os.getenv("dbname")
 
 def main(id):
+
     conn = psycopg2.connect(
-        host=os.getenv("SUPABASE_HOST"),
-        dbname=os.getenv("SUPABASE_DB"),
-        user=os.getenv("SUPABASE_USER"),
-        password=os.getenv("SUPABASE_PASS"),
-        port=5432
+       user=os.getenv('SUPABASE_USER'),
+       password= os.getenv('SUPABASE_PASS'),
+       host=os.getenv('SUPABASE_HOST'),
+       port=6543,
+       dbname=os.getenv('SUPABASE_DB')
     )
     cur = conn.cursor()
+
+    print('cannot connect')
 
     cur.execute('''insert into "testTable" (id, name) 
     values(%s, %s)''', (id, f'test{id}'))
@@ -24,3 +28,4 @@ def main(id):
 
 if __name__ == "__main__":
     main(id_random)
+
