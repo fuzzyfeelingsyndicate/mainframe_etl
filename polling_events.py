e92bafe91382de46026966a92b0d4dffba4a2eab
PHNX-MOD <modith.hadya@gmail.com>
2026-04-17 14:32:12 +0200

    replace pydrive2 with google-api-python-client

--- polling_events.py
+++ polling_events.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
 from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 
 df = pd.DataFrame(
     {
@@ -16,15 +16,14 @@ df = pd.DataFrame(
 scope = ["https://www.googleapis.com/auth/drive"]
 creds_path = os.getenv("GOOGLE_CREDENTIALS_PATH", "credentials.json")
 
-gauth = GoogleAuth()
-gauth.credentials = Credentials.from_service_account_file(creds_path, scopes=scope)
+creds = Credentials.from_service_account_file(creds_path, scopes=scope)
+drive = build("drive", "v3", credentials=creds)
 
-drive = GoogleDrive(gauth)
 filename = "sample.parquet"
 df.to_parquet(filename)
 
-file_drive = drive.CreateFile({"title": filename})
-file_drive.SetContentFile(filename)
-file_drive.Upload()
+file_metadata = {"name": filename}
+media = MediaFileUpload(filename, mimetype="application/octet-stream")
+drive.files().create(body=file_metadata, media_body=media).execute()
 
 print("uploaded file")
